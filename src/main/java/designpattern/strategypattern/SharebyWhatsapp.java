03c25ac5c76dc0e251c11c1f10f925b948839b8a
vanshraj singh <ervanshsingh@gmail.com>
2024-10-10 11:58:53 +0530

    Added strategy design pattern example

---
+++ src/main/java/designpattern/strategypattern/SharebyWhatsapp.java
@@ -0,0 +1,10 @@
+package designpattern.strategypattern;
+
+public class SharebyWhatsapp implements Share {
+
+	@Override
+	public void iShare() {
+		System.out.println("Share by Whatsapp mechanism implemented.");
+	}
+
+}
